06bb451568c3191efe2f60869374e4923da3b8c0
Alan Chan <kalunc@hotmail.com>
2017-10-24 16:28:39 +1100

    Fields and Properties

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -31,7 +31,22 @@ namespace Grades
             grades.Add(grade);
         }
 
-        public string Name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if(!String.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        private string _name;
 
         private List<float> grades;
     }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -11,11 +11,14 @@ namespace Grades
         static void Main(string[] args)
         {
             GradeBook book = new GradeBook();
+            book.Name = "Alan's Gradebook";
+            book.Name = null;
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
